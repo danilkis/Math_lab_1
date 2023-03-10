30dee232a9827584d09bbdc67c43c3c538076d9d
danilkis <danyapancresnicov@gmail.com>
2023-03-10 20:53:52 +0300

    Завершенно

--- Math_lab_1/BisectionMethod.cs
+++ Math_lab_1/BisectionMethod.cs
@@ -14,46 +14,45 @@ public class BisectionMethodSolver {
         _tolerance = 0.001;
     }
 
-    public double Solve() {
+    public List<string> Solve() {
+        List<string> steps = new List<string>();
         double x, fx;
         double fLowerBound = _functionToSolve(_lowerBound);
         double fUpperBound = _functionToSolve(_upperBound);
 
         if (fLowerBound * fUpperBound >= 0)
         {
-            return 0;
+            steps.Add("В данных границах не удалось найти корень\n");
+            steps.Add("Половинчатый метод не может быть применен\n");
+            steps.Add("Решение отмененно\n");
+            return steps;
         }
 
+        int iteration = 0;
         while ((_upperBound - _lowerBound) > _tolerance) {
+            iteration++;
             x = (_lowerBound + _upperBound) / 2;
             fx = _functionToSolve(x);
 
             if (fx == 0.0) {
-                return x;
+                steps.Add($"Корень надйен после {iteration} итераций.\n");
+                steps.Add($"Корень равен: {x}\n");
+                return steps;
             }
             else if (fx * fLowerBound < 0) {
                 _upperBound = x;
                 fUpperBound = fx;
+                steps.Add($"Итерация {iteration} новый верхний предел: {_upperBound}\n");
             }
             else {
                 _lowerBound = x;
                 fLowerBound = fx;
+                steps.Add($"Итерация {iteration} новый нижний предел: {_lowerBound}\n");
             }
         }
 
-        return (_lowerBound + _upperBound) / 2;
-    }
-
-    public List<Tuple<double, double>> GetXYPoints() {
-        List<Tuple<double, double>> points = new List<Tuple<double, double>>();
-        double x = _lowerBound;
-
-        while (x <= _upperBound) {
-            double y = _functionToSolve(x);
-            points.Add(new Tuple<double, double>(x, y));
-            x += _tolerance;
-        }
-
-        return points;
+        steps.Add($"Ответ найден после {iteration} итераций .\n");
+        steps.Add($"Корень равен: {(_lowerBound + _upperBound) / 2}\n");
+        return steps;
     }
 }
